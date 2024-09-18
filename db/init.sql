1b0bff83654e2d48ab2e31f0ca9964954b857ea5
Renato Alves <renatoalves1610@gmail.com>
2024-09-18 09:54:55 -0300

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -4,7 +4,7 @@ USE employees;
 
 CREATE TABLE employee_data (
   Employee_Name VARCHAR(50),
-  position VARCHAR(50)
+  position VARCHAR(50),
   cpf CHAR(11) NOT NULL UNIQUE
 );
 
@@ -12,6 +12,6 @@ CREATE TABLE employee_data (
 INSERT INTO employee_data
   (Employee_Name, Title, cpf)
 VALUES
-  ('Renato Alves', 'Developer', "22233344499");
+  ('Renato Alves', 'Developer', "22233344499"),
   ('Amit Khanna', 'Manager', "11122233399"),
   ('Anjali Gupta', 'Engineer', "33333344499");
